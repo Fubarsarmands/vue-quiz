ad1ac67de0e6cccf4fc55a4fa262abdec738e2c7
Armands Luta <armands.luta@inbox.lv>
2020-08-25 18:22:11 +0300

    izveodju jautajumus

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -2,18 +2,16 @@
     <div>
         <b-nav tabs>
             <b-nav-item disabled><b>Fancy Quiz App</b></b-nav-item>
-            <b-nav-item disabled> Counter: 4/10</b-nav-item>
-
+            <b-nav-item disabled> Counter: {{numCorrect}}/{{numTotal}}</b-nav-item>
         </b-nav>
     </div>
 </template>
 
 <script>
     export default {
-        name: "Header"
+        props: [
+            'numCorrect',
+            'numTotal'
+        ]
     }
 </script>
-
-<style scoped>
-
-</style>
